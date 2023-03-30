deeaf8b03ed447f56b502e559e590652a8d0ace0
jwilli39 <117937537+jwilli39@users.noreply.github.com>
2023-03-30 12:31:49 -0400

    Add files via upload
    This file stores all 68 march madness teams from 2023 into an array list including regular season record.

---
+++ Teams.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+
+public class Teams {
+
+	public static void main(String[] args) {
+		// Create an ArrayList to store teams and their regular season records
+		ArrayList<String> teams = new ArrayList<>();
+
+		// Add teams and their regular season records to the ArrayList
+		teams.add("68. Texas Southern (14-20)");
+		teams.add("67. Southeast Missouri State (19-16)");
+		teams.add("66. Fairleigh-Dickinson (19-15)");
+		teams.add("65. Northern Kentucky (22-12)");
+		teams.add("64. Howard (22-12)");
+		teams.add("63. Texas A&M-CC (23-10)");
+		teams.add("62. Princeton (21-8)");
+		teams.add("61. UNC-Asheville (27-7)");
+		teams.add("60. UCSB (27-7)");
+		teams.add("59. Colgate (26-8)");
+		teams.add("58. Montana State (25-9)");
+		teams.add("57. Kennesaw State (26-8)");
+		teams.add("56. Louisiana (26-7)");
+		teams.add("55. Grand Canyon (24-11)");
+		teams.add("54. Vermont (23-10)");
+		teams.add("53. Arizona State (22-12)");
+		teams.add("52. Kent State (28-6)");
+		teams.add("51. Iona (27-7)");
+		teams.add("50. Pittsburgh (22-11)");
+		teams.add("49. Mississippi State (21-12)");
+		teams.add("48. Nevada (22-10)");
+		teams.add("47. NC State (23-10)");
+		teams.add("46. Charleston (31-3)");
+		teams.add("45. Boise State (24-9)");
+		teams.add("44. Drake (27-7)");
+		teams.add("43. Oral Roberts (30-4)");
+		teams.add("42. Utah State (26-8)");
+		teams.add("41. VCU (27-7)");
+		teams.add("40. Furman (27-7)");
+		teams.add("39. USC (22-10)");
+		teams.add("38. Providence (21-11)");
+		teams.add("37. West Virginia (19-14)");
+		teams.add("36. Penn State (22-13)");
+		teams.add("35. Auburn (20-12)");
+		teams.add("34. Maryland (21-12)");
+		teams.add("33. Memphis (26-8)");
+		teams.add("32. Iowa (19-13)");
+		teams.add("31. Illinois (20-12)");
+		teams.add("30. Northwestern (21-11");
+		teams.add("29. TCU (21-12)");
+		teams.add("28. Creighton (21-12)");
+		teams.add("27. Saint Mary’s (26-7)");
+		teams.add("26. Arkansas (20-13)");
+		teams.add("25. Florida Atlantic (31-3)");
+		teams.add("24. Missouri (24-9)");
+		teams.add("23. Iowa State (19-13)");
+		teams.add("22. Kentucky (21-11)");
+		teams.add("21. Michigan State (19-12)");
+		teams.add("20. Tennessee (23-10)");
+		teams.add("19. San Diego State (27-6)");
+		teams.add("18. Indiana (22-11)");
+		teams.add("17. Miami (25-7)");
+		teams.add("16. Texas A&M (26-8)");
+		teams.add("15. Kansas State (23-9)");
+		teams.add("14. Duke (26-8)");
+		teams.add("13. Xavier (25-9)");
+		teams.add("12. Virginia (25-7)");
+		teams.add("11. Baylor (22-10)");
+		teams.add("10. UConn (25-8)");
+		teams.add("9. Arizona (28-6)");
+		teams.add("8. Gonzaga (28-5)");
+		teams.add("7. UCLA (29-5)");
+		teams.add("6. Marquette (28-6)");
+		teams.add("5. Texas (26-8)");
+		teams.add("4. Purdue (29-5)");
+		teams.add("3. Houston (31-3)");
+		teams.add("2. Kansas (27-7)");
+		teams.add("1. Alabama (29-5)");
+
+		for (int i = 0; i < teams.size(); i++) {
+			System.out.println(teams.get(i));
+
+		}
+	}
+}
