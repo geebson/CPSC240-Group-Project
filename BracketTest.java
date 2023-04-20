c9429555aaf68a568d32bd423d23804daa2708cd
geebson <lukeg23662@gmail.com>
2023-04-19 21:39:47 -0400

    fixed bugs with simulating and writing winners to file

---
+++ BracketTest.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BracketTest {
+
+    @org.junit.jupiter.api.Test
+    void addTeams() {
+
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveWinners() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void simulate() {
+    }
+}
